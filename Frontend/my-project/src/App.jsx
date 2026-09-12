b6371bff828ae308b39fd10cc6e2488edfc1e63d
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 19:41:58 +0500

    Update App.jsx

--- Frontend/my-project/src/App.jsx
+++ Frontend/my-project/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
 import Courses from './Pages/Courses';
-import CourseDetails from './Pages/CourseDetais';
+import CourseDetails from './Pages/CourseDetails';
 import Students from './Pages/Students';
 import Register from './Pages/Register';
 import About from './Pages/About';
