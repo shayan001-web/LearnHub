0ce5fd6c10c37118faad27edb553bfa39606b73a
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 20:51:17 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,12 @@ const contectRoutes = require("./routes/contectRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://learn-hub-frontend-ivory.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use("/api/students", studentRoutes);
