d4bd6181e35619899ac63bac962774cb68955c5d
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 18:35:55 +0500

    Fix API URL in Students component

--- Frontend/my-project/src/Pages/Students.jsx
+++ Frontend/my-project/src/Pages/Students.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import StudentCard from "../Components/StudentCard"
 
-const API_URL = 'https://learn-hub-93dk.vercel.app//students';
+const API_URL = 'https://learn-hub-93dk.vercel.app/students';
 
 function Students() {
   const [students, setStudents] = useState([]);
