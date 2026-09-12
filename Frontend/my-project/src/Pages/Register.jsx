3a3766096a4a67ba111645ae0905088a8e93147d
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 18:33:03 +0500

    Update API_URL to production endpoint

--- Frontend/my-project/src/Pages/Register.jsx
+++ Frontend/my-project/src/Pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = 'http://localhost:5000/api/students';
+const API_URL = 'https://learn-hub-93dk.vercel.app//students';
 const initialForm = {name:'',email:'',age:'',city:'',course:'',phone:''};
 
 function Register() {
