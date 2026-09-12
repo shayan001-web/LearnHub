0e3ce8bd8566b210165849d8d6dd34d2a22a2483
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 18:56:12 +0500

    Update API_URL to new backend endpoint

--- Frontend/my-project/src/Pages/CourseDetails.jsx
+++ Frontend/my-project/src/Pages/CourseDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
-const API_URL = "https://learn-hub-93dk.vercel.app/courses";
+const API_URL = "https://learn-hub-backend-git-main-shayan001-web.vercel.app/api/course";
 
 function CourseDetails() {
   const {id} =  useParams();
