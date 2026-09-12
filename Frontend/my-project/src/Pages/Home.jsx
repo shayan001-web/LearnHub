acf606bcb56f8eb16aab9b14708a749be193339b
shayan001-web <mshayanarain666@gmail.com>
2026-09-12 18:31:32 +0500

    Update API URL to production endpoint

--- Frontend/my-project/src/Pages/Home.jsx
+++ Frontend/my-project/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import CourseCard from '../Components/CourseCard';
 
-const API_URL = 'http://localhost:5000/api/courses';
+const API_URL = 'https://learn-hub-93dk.vercel.app//courses';
 
 function Home(){
     const [courses, setCourses] = useState([]);
